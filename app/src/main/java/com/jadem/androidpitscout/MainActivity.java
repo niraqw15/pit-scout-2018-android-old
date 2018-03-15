cdcec387b69181ca6fd23aa4d2fc521ddff1b5a2
JudeManansala <jademon2002@yahoo.com>
2018-03-14 19:46:20 -0700

    First commit

---
+++ app/src/main/java/com/jadem/androidpitscout/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jadem.androidpitscout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
