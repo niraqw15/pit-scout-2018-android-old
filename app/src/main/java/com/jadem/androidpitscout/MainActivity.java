e7ddf753186e382aac3a19cf95a2375fbfccf307
JudeManansala <jademon2002@yahoo.com>
2018-03-14 20:45:40 -0700

    Begin work on listView in mainActivity

--- app/src/main/java/com/jadem/androidpitscout/MainActivity.java
+++ app/src/main/java/com/jadem/androidpitscout/MainActivity.java
@@ -2,12 +2,26 @@ package com.jadem.androidpitscout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+ListView listView;
+ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1)
+        listView = (ListView) findViewById(R.id.teamsList);
+        listView.setAdapter(adapter);
+        updateListView();
+    }
+
+    private void updateListView() {
+
     }
 }
