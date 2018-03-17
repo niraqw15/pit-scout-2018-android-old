e7a70e2a428024d65f61adb01fc74d8f3d9c70c8
niraqw15 <niraqw@gmail.com>
2018-03-17 13:41:08 -0700

    Add onClicks for all buttons in TimerActivity

--- app/src/main/java/com/jadem/androidpitscout/TimerActivity.java
+++ app/src/main/java/com/jadem/androidpitscout/TimerActivity.java
@@ -2,6 +2,10 @@ package com.jadem.androidpitscout;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 
 /**
  * Created by niraq on 3/15/2018.
@@ -9,12 +13,36 @@ import android.support.v7.app.AppCompatActivity;
 
 public class TimerActivity extends AppCompatActivity {
 
+    private boolean isRamp;
+    private Switch timerTypeSwitch;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_timer);
+
+        timerTypeSwitch = (Switch) findViewById(R.id.timerSwitch);
+        isRamp = timerTypeSwitch.isChecked();
+        timerTypeSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                isRamp = isChecked;
+                //TODO: Change what the listview displays (and update it?)
+            }
+        });
 
         //TODO: Add Firebase refs
+    }
+
+    public void confirmTimer(View view) {
+        //TODO: Complete
+    }
+
+    public void cancelTimer(View view) {
+        //TODO: Complete
+    }
+
+    public void switchTimerType(View view) {
+        //TODO: Complete
     }
 
 }
