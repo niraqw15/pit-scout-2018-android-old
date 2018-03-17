34c5ee4c6b8015c1cff5f3cc383fa9cd4c077646
niraqw15 <niraqw@gmail.com>
2018-03-17 15:02:27 -0700

    Add onClick for start timer button

--- app/src/main/java/com/jadem/androidpitscout/TimerActivity.java
+++ app/src/main/java/com/jadem/androidpitscout/TimerActivity.java
@@ -2,13 +2,19 @@ package com.jadem.androidpitscout;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 /**
  * Created by niraq on 3/15/2018.
@@ -16,18 +22,24 @@ import com.google.firebase.database.FirebaseDatabase;
 
 public class TimerActivity extends AppCompatActivity {
 
-    private boolean isRamp;
+    private boolean isRamp, timerRunning;
+    private int teamNumber;
     private Switch timerTypeSwitch;
     private FirebaseDatabase database;
     private DatabaseReference myRef;
+    private ValueEventListener trialEventListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
 
+        teamNumber = 1;//TODO: Temporary for testing, remove when done
+
+        timerRunning = false;
+
         database = FirebaseDatabase.getInstance();
-        myRef = database.getReference().child();
+        myRef = database.getReference().child("Teams").child("" + teamNumber); //TODO: Recieve team number before doing this!
 
         timerTypeSwitch = (Switch) findViewById(R.id.timerSwitch);
         isRamp = timerTypeSwitch.isChecked();
@@ -38,7 +50,26 @@ public class TimerActivity extends AppCompatActivity {
             }
         });
 
-        //TODO: Add Firebase refs
+        trialEventListener = new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if(!dataSnapshot.child("Messages").getValue().equals(null)) {
+
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("Error", "ChatRoomEventListener Cancelled");
+                Toast connectionErrorToast = Toast.makeText(getApplicationContext(), "Connection Error", Toast.LENGTH_SHORT);
+                connectionErrorToast.setGravity(Gravity.CENTER, 0, 0);
+                connectionErrorToast.show();
+            }
+        };
+    }
+
+    public void toggleTimer(View view) {
+        //TODO: Complete
     }
 
     public void confirmTimer(View view) {
