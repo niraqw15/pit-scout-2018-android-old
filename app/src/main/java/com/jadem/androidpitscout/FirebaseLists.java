d044b522d071988efc8383df4e6af09216268fa6
JudeManansala <jademon2002@yahoo.com>
2018-03-17 10:11:28 -0700

    Add first UI components

---
+++ app/src/main/java/com/jadem/androidpitscout/FirebaseLists.java
@@ -0,0 +1,10 @@
+package com.jadem.androidpitscout;
+
+import com.jadem.androidpitscout.firebase_classes.Match;
+/**
+ * Created by jadem on 3/17/2018.
+ */
+
+public class FirebaseLists {
+    public static FirebaseList<Match> matchesList;
+}
