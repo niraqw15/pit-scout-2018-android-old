13dbf6cf8b4b5b7406f5604675e4416414302342
niraqw15 <niraqw@gmail.com>
2018-03-28 13:53:00 -0700

    Initialize several variables in MainActivity

--- app/src/main/java/com/jadem/androidpitscout/MainActivity.java
+++ app/src/main/java/com/jadem/androidpitscout/MainActivity.java
@@ -1,7 +1,9 @@
 package com.jadem.androidpitscout;
 
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -9,16 +11,21 @@ import android.text.TextWatcher;
 
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
 
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
     ArrayAdapter<String> searchAdapter;
+    Context context;
     Button tempButton;
     EditText searchBar;
+    BaseAdapter adapter;
+    ListView listView;
     public static FirebaseDatabase dataBase;
     public static DatabaseReference ref;
 
